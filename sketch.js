f0a9764096becc046a6fdcc56fdd60fa58c4417e
SentientPineTree <79885510+allhailtheraccoon@users.noreply.github.com>
2023-05-08 18:11:23 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,21 @@ let smileyy = 0;
 let smileyxv= 0;
 let smileyyv= 0;
 var sound1;
+var sound2;
+let rows, cols, vectors;
+let px = [];
+let py = [];
+let pc = [];
+let ps = [];
+let nump = 1500; //number of points
+let sc = 2;
+
 function preload(){
-  sound1 = loadSound("bell.wav")
+  sound1 = loadSound("bell.wav");
+  sound2 = loadSound("fog.mp3");
+  //img = loadImage('');
 }
+
 function setup() {
   createCanvas(3.4*400, 3.4*200);
   strokeWeight(1);
@@ -15,6 +27,15 @@ function setup() {
   noFill();
   smileyx = random(0,3.4*400);
   smileyy = random(0,3.4*400);
+  rows = height / 20;
+  cols = width / 20;
+  vectors = create2DArray(rows, cols);
+  for(let i =0; i<nump; i++){
+    px.push(random(0,1));
+    py.push(random(0,1));
+    pc.push(0);
+    ps.push(random(0,100));
+  }
 }
 
 function draw() {
@@ -53,32 +74,84 @@ function draw() {
     fill((0+360*(sin(t)+1)/2)%360,100,100);
     text("double-click to go forward",-54,0);
     fill(0);
-  }
+  }//opening page
+
+
   if(state==1){
-    background(10);
-    
-    
-    fill(10);
+    background(10);    
+    fill(185,100,40);
     rect(20,20,3.4*400 - 40,3.4*200 - 40);
-    let canvas = document.getElementsByTagName("canvas")[0];
+    
+    let canvas = document.getElementsByTagName("canvas")[0];canvas.style.cursor = "";
+    
     fill(120,100,100);
     rect(100,100,100,100);
     stroke(0,100,100);
     fill(50,100,100);
-    
-    canvas.style.cursor = "";
     if(mouseX>100){if(mouseX<200){if(mouseY>100){if(mouseY<200){
       canvas.style.cursor = "pointer";
       stroke(300,100,100);
       fill(250,100,100);
       if(mouseIsPressed==1){
         canvas.style.cursor = "";
-        state++;
+        state=2;
+        sound1.play();
+      }
+    }}}}
+    textSize(19);
+    text("shag carpet",100,156);
+    
+    fill(120,100,100);
+    rect(width-200,100,100,100);
+    stroke(0,100,100);
+    fill(50,100,100);
+    if(mouseX>width-200){if(mouseX<width-100){if(mouseY>100){if(mouseY<200){
+      canvas.style.cursor = "pointer";
+      stroke(300,100,100);
+      fill(250,100,100);
+      if(mouseIsPressed==1){
+        canvas.style.cursor = "";
+        state=3;
         sound1.play();
       }
     }}}}
     textSize(30);
-    text("Button1",100,160);
+    text("Button2",width-202,160);
+    
+    fill(120,100,100);
+    rect(width-200,height-200,100,100);
+    stroke(0,100,100);
+    fill(50,100,100);
+    if(mouseX>width-200){if(mouseX<width-100){if(mouseY>height-200){if(mouseY<height-100){
+      canvas.style.cursor = "pointer";
+      stroke(300,100,100);
+      fill(250,100,100);
+      if(mouseIsPressed==1){
+        canvas.style.cursor = "";
+        state=4;
+        sound1.play();
+      }
+    }}}}
+    textSize(30);
+    text("Button3",width-202,height-140);    
+
+    fill(120,100,100);
+    rect(100,height-200,100,100);
+    stroke(0,100,100);
+    fill(50,100,100);
+    if(mouseX>100){if(mouseX<200){if(mouseY>height-200){if(mouseY<height-100){
+      canvas.style.cursor = "pointer";
+      stroke(300,100,100);
+      fill(250,100,100);
+      if(mouseIsPressed==1){
+        canvas.style.cursor = "";
+        state=5;
+        sound1.play();
+      }
+    }}}}
+    textSize(30);
+    text("Button4",100,height-140);
+
     strokeWeight(4);
     fill(60,100,100);
     stroke(350,100,100);
@@ -122,16 +195,75 @@ function draw() {
         }
       }
     }
-  }
+  }//hungry smiley face four way split
+
+
   if(state==2){
-    background(150,100,100);
+    sound2.play();
+    //background(150,100,100);
+    fill(0,0,100,0.05);
+    rect(0,0,width,height)
+    // Update vectors based on noise
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        let xoff = j / cols;
+        let yoff = i / rows;
+        let angle = noise(xoff*sc, yoff*sc, frameCount * 0.001) * TWO_PI * 4;
+        vectors[i][j] = p5.Vector.fromAngle(angle);
+        vectors[i][j].mult(10);
+      }
+    }
+    for(var i=0; i<nump; i++){
+      let angle2 = 1*noise(px[i]*sc, py[i]*sc, frameCount * 0.001) * TWO_PI * 4; 
+      angle2 += 4.1*TAU*pow(dist(px[i]*2,py[i],mouseX/width*2,mouseY/height),0.25);
+      px[i]+=cos(angle2)/500;
+      py[i]+=sin(angle2)/500;
+      px[i] = px[i]%1;
+      py[i] = py[i]%1;
+      strokeWeight(5);
+      stroke(0,100,0);
+      //print(angle2);
+      point(width*px[i],height*py[i]);
+    }
+    for(var i = 0; i<nump/30; i++){
+      px[floor(random(0,nump))] = random(0,1);
+      py[floor(random(0,nump))] = random(0,1);
+    }
+  }//shag carpet
+
+
+  if(state==3){
+    background(100,100,100);
   }
+
+  
+  if(state==4){
+    background(50,100,100);
+  }
+
+
+  if(state==5){
+    background(200,100,100);
+  }
+
+
   t += 0.01;
   print(state);
 }
+
+
 function doubleClicked() {
   if(state<1){
     state++;
     sound1.play();
   }
+}
+
+
+function create2DArray(rows, cols) {
+  let arr = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    arr[i] = new Array(cols);
+  }
+  return arr;
 }
